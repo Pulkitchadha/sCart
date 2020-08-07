8f17018d942ef3f4225863eeb4e48726758cb4fb
pulkitchadha <pulkitchadha27@gmail.com>
2020-08-07 17:45:37 +0530

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import Login from './pages/Login';
+import ProductList from './pages/ProductList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login"><Login /></Route>
+                <Route path="/products"><ProductList /></Route>
+                <Route path="*"><Login /></Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/ProductList/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ProductList extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Product List Page</h1>
+            </div>
+        )
+    }
+}
