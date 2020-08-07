269b1a95b94706bd103b94bae453174a98a03a2e
pulkitchadha <pulkitchadha27@gmail.com>
2020-08-07 18:54:51 +0530

    completed login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 import Login from './pages/Login';
 import ProductList from './pages/ProductList';
@@ -8,9 +8,9 @@ function App() {
     return (
         <Router>
             <Switch>
-                <Route path="/login"><Login /></Route>
-                <Route path="/products"><ProductList /></Route>
-                <Route path="*"><Login /></Route>
+                <Route path="/login" component={Login} />
+                <Route path="/products" component={ProductList} />
+                <Route path="*"><Redirect to="/login" /></Route>
             </Switch>
         </Router>
     );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,57 @@
-import React, { Component } from 'react';
+import React from 'react';
+import api from 'services/api';
+import { showMessage } from 'services/utility';
 
-export default class Login extends Component {
+export default class Login extends React.Component {
+    state = {
+        username: '',
+        password: '',
+    }
+
+    onUpdateFormValue = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    login = async (e) => {
+        try {
+            e.preventDefault();
+            const { username, password } = this.state;
+
+            if (!username) throw new Error('Username can not be empty');
+            if (!password) throw new Error('Password can not be empty');
+
+            const result = await api.get('users', { username, password });
+
+            if (result?.length && result[0]?.password !== password) throw new Error('Invalid Password');
+
+            this.props.history.push('/products');
+        } catch (err) {
+            showMessage('error', err.message);
+        }
+    }
+
     render() {
         return (
             <div className="App">
-                <h1>Login Page</h1>
+                <h1>sCart</h1>
+                <input
+                    className="input"
+                    name="username"
+                    type="text"
+                    placeholder="username"
+                    value={this.state.username}
+                    onChange={this.onUpdateFormValue}
+                /><br />
+                <input
+                    className="input"
+                    name="password"
+                    type="password"
+                    placeholder="password"
+                    value={this.state.password}
+                    onChange={this.onUpdateFormValue}
+                /><br /><br />
+                <button className="login-btn" onClick={this.login}>Login</button>
             </div>
         )
     }
