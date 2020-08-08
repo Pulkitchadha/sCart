2625046997f6e597b472674f9fae43fb07d28937
pulkitchadha <pulkitchadha27@gmail.com>
2020-08-08 17:39:57 +0530

    added search products action in products store

--- src/store/products/actions.js
+++ src/store/products/actions.js
@@ -1,5 +1,12 @@
 import api from 'services/api';
-import { GET_PRODUCTS_BEGIN, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_FAIL } from "./types";
+import {
+    GET_PRODUCTS_BEGIN,
+    GET_PRODUCTS_SUCCESS,
+    GET_PRODUCTS_FAIL,
+    SEARCH_BEGIN,
+    SEARCH_SUCCESS,
+    SEARCH_FAIL
+} from "./types";
 
 export const getProducts = () => async (dispatch) => {
     try {
@@ -10,5 +17,17 @@ export const getProducts = () => async (dispatch) => {
         dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
     } catch (err) {
         dispatch({ type: GET_PRODUCTS_FAIL, payload: err.message });
+    }
+}
+
+export const searchProducts = (text) => async (dispatch) => {
+    try {
+        dispatch({ type: SEARCH_BEGIN });
+
+        const products = await api.get('products', { title: text });
+
+        dispatch({ type: SEARCH_SUCCESS, payload: products })
+    } catch (err) {
+        dispatch({ type: SEARCH_FAIL, payload: err.message });
     }
 }
--- src/store/products/index.js
+++ src/store/products/index.js
@@ -2,6 +2,9 @@ import {
     GET_PRODUCTS_BEGIN,
     GET_PRODUCTS_SUCCESS,
     GET_PRODUCTS_FAIL,
+    SEARCH_BEGIN,
+    SEARCH_SUCCESS,
+    SEARCH_FAIL
 } from './types';
 
 const INITIAL_STATE = {
@@ -25,6 +28,24 @@ export default function (state = INITIAL_STATE, { type, payload }) {
                 products: payload
             }
         case GET_PRODUCTS_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: payload
+            }
+        case SEARCH_BEGIN:
+            return {
+                ...state,
+                loading: true,
+                error: null
+            }
+        case SEARCH_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                products: payload
+            }
+        case SEARCH_FAIL:
             return {
                 ...state,
                 loading: false,
