2dae6ef6a0190721a87ae16bfe0177d40b4f15a0
pulkitchadha <pulkitchadha27@gmail.com>
2020-08-09 18:30:31 +0530

    added color in product card component

--- src/pages/ProductList/components/ProductCard.js
+++ src/pages/ProductList/components/ProductCard.js
@@ -11,14 +11,14 @@ function ProductCard({ product, onAdd, onDelete, isAdded }) {
                     <div className="col-sm-8">
                         <ul className="list-group list-group-flush">
                             {/* <li className="list-group-item">{product?.title || 'NA'}</li> */}
-                            <li className="list-group-item">{product?.brand || 'NA'}</li>
-                            <li className="list-group-item">$ {product?.price?.final_price || 'NA'}</li>
-                            <li className="list-group-item">{product?.discount || 'NA'} %</li>
+                            <li className="list-group-item">{product?.brand}</li>
+                            <li className="list-group-item">$ {product?.price?.final_price}</li>
+                            <li className="list-group-item">{product?.discount} %</li>
                         </ul>
                     </div>
                     <div className="col-sm-4">
                         <ul className="list-group list-group-flush">
-                            <li className="list-group-item">Color</li>
+                            <li className="list-group-item">{product?.colour?.title}</li>
                             {!isAdded ? <li className="list-group-item text-success" onClick={() => onAdd(product)}>Add (+)</li>
                                 :
                                 <li className="list-group-item text-danger" onClick={() => onDelete(product.id)}>Remove (-)</li>
